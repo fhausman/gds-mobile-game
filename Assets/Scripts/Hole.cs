24aae78410f4be8a76d60ff45770a8d86ca831d7
Filip Hausman <filip.hausman@gmail.com>
2020-02-01 23:03:59 +0100

    hole is moving

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public float MoveSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.left * MoveSpeed * Time.deltaTime / 100.0f;
+    }
+}
