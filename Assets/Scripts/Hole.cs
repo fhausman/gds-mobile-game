2e63daaad2341bc795256bd4ecc97ae2182a3fbb
Filip Hausman <filip.hausman@gmail.com>
2020-02-01 23:37:47 +0100

    primitive hole management

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Hole : MonoBehaviour
 {
-    public float MoveSpeed = 10.0f;
+    public float MoveSpeed = 100.0f;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/HoleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleManager : MonoBehaviour
+{
+    public GameObject holePrefab;
+    public float speedDelta = 50.0f;
+    public Vector3 spawnPoint = new Vector3(11.03f, -3.99f, 0.0f);
+
+    private uint spawnedCount = 0;
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "Hole")
+        {
+            Debug.Log("Hole left the stage");
+            Destroy(col.gameObject);
+
+            var newHole = Instantiate(holePrefab);
+            newHole.transform.position = spawnPoint;
+            newHole.GetComponent<Hole>().MoveSpeed += speedDelta * spawnedCount;
+
+            spawnedCount++;
+        }
+    }
+}
