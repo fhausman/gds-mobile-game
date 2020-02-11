a72abcd3e9ab5d5c1b731308b12da2a0c15b7a86
Filip Hausman <filip.hausman@gmail.com>
2020-02-11 21:46:25 +0100

    new drawing arc script

--- Assets/Scripts/ArcLine.cs
+++ Assets/Scripts/ArcLine.cs
@@ -1,64 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class ArcLine : MonoBehaviour
 {
     private LineRenderer lr;
-    private float gravity;
-    private int positionCount { get => resolution + 1; }
-    private float radianAngle { get => Mathf.Deg2Rad * angle; }
+    private Vector3 startPosition;
+    private Vector3 endPosition = new Vector3(0.0f, 5.0f);
 
-    public float velocity;
-    public float angle;
     public int resolution;
+    public float range = 0.0f;
 
     void Awake()
     {
         lr = GetComponent<LineRenderer>();
-        gravity = Mathf.Abs(Physics2D.gravity.y);
+        startPosition = GetComponentInParent<Transform>().position;
     }
 
     void Update()
     {
-        Debug.Log(velocity);
         RenderArc();
     }
 
     void RenderArc()
     {
-        if (velocity > 0.0f)
+        if (range > 0.0f)
         {
             lr.enabled = true;
-            lr.positionCount = positionCount;
             lr.SetPositions(GetArcPoints());
         }
-        else
-        {
-            lr.enabled = false;
-        }
+        //else
+        //{
+        //    lr.enabled = false;
+        //}
     }
 
     Vector3[] GetArcPoints()
     {
-        var points = new Vector3[positionCount];
-        var maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle) / gravity);
+        var points = new Vector3[resolution];
+        var delta = (endPosition.y + startPosition.y) / (float)resolution;
 
-        for (int i = 0; i < positionCount; ++i)
+        for (int i = 0; i < resolution; ++i)
         {
-            var delta = i / (float)resolution;
-            points[i] = CalculatePoint(delta, maxDistance*1.8f);
+            points[i] = CalculatePoint(delta*i);
         }
 
         return points;
     }
 
-    Vector3 CalculatePoint(float delta, float maxDistance)
+    Vector3 CalculatePoint(float delta)
     {
-        var x = delta * maxDistance;
-        var y = x * Mathf.Tan(radianAngle) - ((gravity * x * x) / (2 * velocity * velocity * Mathf.Cos(radianAngle) * Mathf.Cos(radianAngle)));
+        var y = delta;
+        var x = Mathf.Sqrt(y) * range;
         
-        return new Vector3(x, y);
+        return new Vector3(x, -y + startPosition.y);
     }
 }
