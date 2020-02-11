4d3cfb71e3697c21f9fd89e140833ee866aaf044
Filip Hausman <filip.hausman@gmail.com>
2020-02-11 23:40:22 +0100

    exploding projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public bool shouldDestroy = false;
+
+    private Animation anim;
+    private void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        if (shouldDestroy)
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        anim["Explode"].speed = 6.0f;
+        anim.Play("Explode");
+    }
+}
