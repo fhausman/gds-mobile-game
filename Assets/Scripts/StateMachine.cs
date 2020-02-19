231d34926b58f9269c409afbc2ccabe60c81b52d
Filip Hausman <filip.hausman@gmail.com>
2020-02-19 23:16:40 +0100

    state machine added

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+public interface IState
+{
+    public void Init();
+    public void Update();
+    public void Exit();
+}
+
+public class EmptyState : IState
+{
+    public void Exit()
+    {
+    }
+
+    public void Init()
+    {
+    }
+
+    public void Update()
+    {
+    }
+}
+
+public class StateMachine
+{
+    IState currentState = new EmptyState();
+    Dictionary<int, IState> states = new Dictionary<int, IState>();
+
+    public void AddState(int id, IState state)
+    {
+        states.Add(id, state);
+    }
+
+    public void ChangeState(int id)
+    {
+        if(!states.ContainsKey(id))
+        {
+            throw new System.Exception(string.Format("Invalid state requested! {0}", id));
+        }
+
+        currentState.Exit();
+        currentState = states[id];
+        currentState.Init();
+    }
+
+    public void Update()
+    {
+        currentState.Update();
+    }
+}
