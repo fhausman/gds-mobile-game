d627dadee03717360cf36d7c9c6055b6f9a5a214
Filip Hausman <filip.hausman@gmail.com>
2020-03-04 00:17:54 +0100

    scirptable spellbook

---
+++ Assets/Scripts/Spellbook.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
+
+public class Spellbook : MonoBehaviour
+{
+    [System.Serializable]
+    public struct SpellData
+    {
+        public int basePrice;
+        public int multiplier;
+        public int buyCount;
+        public Button bookButton;
+        public Button castButton;
+    }
+
+    public SpellData lilithsBlessing;
+    public SpellData praiseSatan;
+    public SpellData unholyChant;
+    public SpellData fightFireWithFire;
+
+    private int mana = 0;
+
+    void Start()
+    {
+    }
+
+    public void LilithsBlessing()
+    {
+        lilithsBlessing.castButton.interactable = false;
+    }
+
+    public void PraiseSatan()
+    {
+        praiseSatan.castButton.interactable = false;
+    }
+
+    public void UnholyChant()
+    {
+        unholyChant.castButton.interactable = false;
+    }
+
+    public void FightFireWithFire()
+    {
+        fightFireWithFire.castButton.interactable = false;
+    }
+}
