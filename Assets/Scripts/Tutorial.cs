1d1c2e12b2dbfc44ac8cabb95616ccdbf3712a91
Filip Hausman <filip.hausman@gmail.com>
2020-03-16 01:50:46 +0100

    few bugs fixed

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -147,12 +147,12 @@ public class PriestTutorialState : IState
     {
         tut.spawnPointLeft.SpawnPriest();
 
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
 
         checkState = true;
         tut.spawnPointLeft.SpawnMob(SpawnPoint.Enemies.BasicMob);
 
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(2.0f);
 
         GameManager.acceptsPlayerInput = true;
     }
