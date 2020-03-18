58be120b3aaf4e5961a502b7f5ce66c3485b29ee
Filip Hausman <filip.hausman@gmail.com>
2020-03-18 21:25:49 +0100

    death fire

--- Assets/Scripts/Enemies/Torch.cs
+++ Assets/Scripts/Enemies/Torch.cs
@@ -7,6 +7,7 @@ public class Torch : MonoBehaviour
     private Scorcher scorcher;
     private Scorcher subScorcher;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 
     public bool active { get; private set; } = true;
 
@@ -20,17 +21,19 @@ public class Torch : MonoBehaviour
         subScorcher = new Scorcher(fire, fire.GetComponent<SpriteRenderer>().material);
 
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     public void Hit()
     {
-        Schorche(0.1f);
+        Schorche(0.05f);
     }
 
     public void Schorche(float time)
     {
         scorcher.scorchTime = time;
         subScorcher.scorchTime = time;
+        sr.color = new Color(0xFF, 0xAE, 0x00);
 
         active = false;
         StartCoroutine(subScorcher.ScorchAndDestroy(false));
